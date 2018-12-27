b3e390a5b60aff7510c56a0667db31a75bc26f17
Benjamin Taillon <taillonbenjamin@gmail.com>
2018-12-27 18:05:23 -0500

    Cleanup of the manage screen.

--- lib/pages/manage/screen.dart
+++ lib/pages/manage/screen.dart
@@ -15,29 +15,6 @@ class ManageScreen extends StatefulWidget {
 }
 
 class _ManageScreenState extends State<ManageScreen> {
-  _ManageScreenState(Session session) : _session = session, _fragments = [
-    FragmentDefinition(
-      "Home",
-      Icons.home,
-      Builder(
-        builder: (context) {
-          return HomeFragment();
-        }
-      )
-    ),
-    FragmentDefinition(
-      "Services",
-      Icons.list,
-      Builder(
-        builder: (context) {
-          return ServicesFragment(session);
-        }
-      )
-    ),
-  ] {
-    _currentFragment = _fragments[0];
-  }
-
   // Objects
   final List<FragmentDefinition> _fragments;
   final Session _session;
@@ -45,66 +22,99 @@ class _ManageScreenState extends State<ManageScreen> {
   // States
   FragmentDefinition _currentFragment;
 
+  // Properties
+  Drawer get _drawer {
+    return Drawer(
+      child: Column(
+        children: [
+          DrawerHeader(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    IconButton(
+                      icon: Icon(
+                        Icons.exit_to_app,
+                        color: Colors.white,
+                      ),
+                      onPressed: _exitScreen,
+                    ),
+                  ]
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      _session.username,
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ]
+                ),
+              ]
+            ),
+            decoration: BoxDecoration(
+              color: ThemeData.light().primaryColor
+            ),
+          ),
+          Column(
+            children: _fragments.map((fragment) => ListTile(
+              leading: Icon(fragment.icon),
+              title: Text(fragment.name),
+              selected: fragment == _currentFragment,
+              onTap: () {
+                setState(() {
+                  _currentFragment = fragment;
+                });
+                Navigator.of(context).pop();
+              }
+            )).toList()
+          )
+        ]
+      )
+    );
+  }
+
+  _ManageScreenState(Session session) : 
+    _session = session,
+    _fragments = [
+      FragmentDefinition(
+        "Home",
+        Icons.home,
+        Builder(
+          builder: (context) {
+            return HomeFragment();
+          }
+        )
+      ),
+      FragmentDefinition(
+        "Services",
+        Icons.list,
+        Builder(
+          builder: (context) {
+            return ServicesFragment(session);
+          }
+        )
+      ),
+    ]
+  {
+    _currentFragment = _fragments[0];
+  }
+
+  void _exitScreen() {
+    Navigator.of(context)..pop()..pop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Manage MAGNET"),
       ),
-      drawer: Drawer(
-        child: Column(
-          children: [
-            DrawerHeader(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      IconButton(
-                        icon: Icon(
-                          Icons.exit_to_app,
-                          color: Colors.white,
-                        ),
-                        onPressed: () {
-                          Navigator.of(context)..pop()..pop();
-                        },
-                      ),
-                    ]
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        _session.username,
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ]
-                  ),
-                ]
-              ),
-              decoration: BoxDecoration(
-                color: ThemeData.light().primaryColor
-              ),
-            ),
-            Column(
-              children: _fragments.map((fragment) => ListTile(
-                leading: Icon(fragment.icon),
-                title: Text(fragment.name),
-                selected: fragment == _currentFragment,
-                onTap: () {
-                  setState(() {
-                    _currentFragment = fragment;
-                  });
-                  Navigator.of(context).pop();
-                }
-              )).toList()
-            )
-          ]
-        )
-      ),
+      drawer: _drawer,
       body: _currentFragment.fragment,
     );
   }
